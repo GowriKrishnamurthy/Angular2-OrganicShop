0467a0da4e0b3c96525ac27ac262255d1a31e153
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-15 11:16:05 +1000

    Implementing Google login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as firebase from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth'; 
+
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
@@ -7,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private angularFireAuth: AngularFireAuth) { }
 
   ngOnInit() {
   }
-
+  onLogin(){
+    this.angularFireAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
 }
