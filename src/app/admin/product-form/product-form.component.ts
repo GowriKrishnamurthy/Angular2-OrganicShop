282cc113537ba7fc4829a120fab469b9bc488b5e
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-19 11:55:09 +1000

    Router navigation to main product page upon successful new production creation

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../category.service';
 import { ProductService } from '../product.service';
 import { Form } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -13,15 +14,17 @@ export class ProductFormComponent implements OnInit {
   // Observable to get the categories from Firebase Db
   categories$;
 
-  constructor( categoryService: CategoryService,
-  private productService:ProductService) {
+  constructor(categoryService: CategoryService,
+      private productService: ProductService,
+      private router:Router) {
     this.categories$ = categoryService.getCategories();
   }
 
-  onSave(product){
+  onSave(product) {
     this.productService.createProduct(product);
+    this.router.navigate(['/admin/products']);
   }
   ngOnInit() {
   }
-  
+
 }
