1079365b7e6e19b9aeacf2b34d51a4467e86562f
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-28 12:20:44 +1000

    Number of shopping cart items in navbar.

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../Auth/auth.service';
 import { AppUser } from '../Auth/user.model';
+import { ShoppingCartService } from '../shared/services/shopping-cart.service';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
+  // Fetch App user from Auth service
+  appUser: AppUser;
+  shoppingCartItemCount: number;
+
+  constructor(private authService: AuthService,
+    private shoppingCartService: ShoppingCartService) {
+  }
+
+  // await cant be done in constructor, so implementing this in ngOnInit.
+  // get the current cart 
+  async ngOnInit() {
+    this.authService.appUser$.subscribe(appUser => this.appUser = appUser)
+    const cart$ = (await this.shoppingCartService.getCart()).snapshotChanges();
+
+    cart$.subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+      for (let productId in cart.payload.val().items) {
+        this.shoppingCartItemCount += cart.payload.val().items[productId].quantity
+      }
+    });
+  }
+  onLogout() {
+    this.authService.logout();
+  }
+}
   // Getting the current logon - Option 1
   /* currentUser: firebase.User;
   constructor(private angularFireAuth: AngularFireAuth) {
@@ -27,13 +53,3 @@ export class BsNavbarComponent {
     this.currentUser$ = this.angularFireAuth.authState;
   }
 */
-  // Fetch App user from Auth service
-  appUser: AppUser;
-  constructor(private authService: AuthService) {
-    this.authService.appUser$.subscribe(appUser => this.appUser = appUser)
-  }
-  onLogout() {
-    this.authService.logout();
-  }
-
-}
